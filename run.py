bdfe2d82c3cfb96a0837ecc0452a67f05f5fb6ee
pedrobernardi <p3drobernardi@gmail.com>
2025-03-25 05:14:32 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+import os
+import uvicorn
+from main import app  # ou o nome real do seu arquivo
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
